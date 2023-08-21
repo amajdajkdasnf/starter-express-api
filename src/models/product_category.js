58900e07c7d751a72b1e99597b1d85d5fd843ea5
mino-cyber <67506601+mino-cyber@users.noreply.github.com>
2023-08-21 18:52:26 +0300

    update

---
+++ src/models/product_category.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const categorySchema = new Schema({
+    name:{
+        type:String
+    },
+    image:{
+        type: String
+    },
+    desc: {
+        type: String
+    },
+}, { timeseries: true })
+
+
+const Product_category = mongoose.model('product_catagory', categorySchema)
+module.exports = Product_category
